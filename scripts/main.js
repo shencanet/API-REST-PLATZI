bebeb76895140c464311c225aa7c4d453850ac9e
shen <santi.dionis@gmail.com>
2023-03-05 20:06:46 +0100

    api-cats 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 
-async function reload () {
+async function reload() {
     const res = await fetch(URL);
     const data = await res.json();
 
@@ -14,5 +14,4 @@ async function reload () {
 
 }
 
-const myButton = document.querySelector("button");
-myButton.onclick = reload;
+
