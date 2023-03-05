5b645949e560b4417251dd0b222ccd0824a69de6
shen <santi.dionis@gmail.com>
2023-03-05 19:28:46 +0100

    api-cats

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 
-async function myCat () {
+async function reload () {
     const res = await fetch(URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    const img1 = document.querySelector('img1');
+    const img2 = document.querySelector('img2');
+    const img3 = document.querySelector('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+
 }
 
 const myButton = document.querySelector("button");
-myButton.onclick = myCat;
+myButton.onclick = reload;
