0721ca41e3eb7281a91d85c2330eba0c2f8605e6
shen <santi.dionis@gmail.com>
2023-03-05 20:44:34 +0100

    api-cats 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
-
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 
 async function reload() {
-    const res = await fetch(URL);
-    const data = await res.json();
+  const res = await fetch(API_URL);
+  const data = await res.json();
 
-    const img1 = document.querySelector('.img1');
-    const img2 = document.querySelector('.img2');
-    const img3 = document.querySelector('.img3');
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
-
+  console.log(data)
+  const img1 = document.getElementById('img1');
+  const img2 = document.getElementById('img2');
+  const img3 = document.getElementById('img3');
+  
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
 }
 
-
+reload();
