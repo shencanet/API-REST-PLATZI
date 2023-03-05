842112ae1b9bfd56004f874a014b50ebf913b3bc
shen <santi.dionis@gmail.com>
2023-03-05 19:43:20 +0100

    api-cats

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ async function reload () {
     const res = await fetch(URL);
     const data = await res.json();
 
-    const img1 = document.querySelector('img1');
-    const img2 = document.querySelector('img2');
-    const img3 = document.querySelector('img3');
+    const img1 = document.querySelector('.img1');
+    const img2 = document.querySelector('.img2');
+    const img3 = document.querySelector('.img3');
     img1.src = data[0].url;
     img2.src = data[1].url;
     img3.src = data[2].url;
