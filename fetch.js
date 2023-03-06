b106589003b6171dd607b7d05a2e94f94d671661
shen <santi.dionis@gmail.com>
2023-03-06 20:30:55 +0100

    api-cats Headers

---
+++ fetch.js
@@ -0,0 +1,32 @@
+//axios
+//trae.js
+//node-fetch
+//request
+/*
+const api = axios.create({
+	baseURL: 'https://api.thecatapi.com/v1',
+	headers: {'X-API-KEY': 'YOUR-KEY'}
+}); 
+const https = require('https');
+
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
+ */
