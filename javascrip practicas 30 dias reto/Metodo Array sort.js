b69d2c9acc4bb1e2d25cbd1e4095b058b834a9c1
shen <santi.dionis@gmail.com>
2023-03-10 20:34:55 +0100

    ADD: Sort

--- javascrip practicas 30 dias reto/Metodo Array sort.js
+++ javascrip practicas 30 dias reto/Metodo Array sort.js
@@ -35,3 +35,22 @@ const people = [
     console.log(people);
     
     // Output: [{ name: "Jane", age: 28 }, { name: "John", age: 30 }, { name: "Jim", age: 32 }]
+    /*Intentos
+
+En este desafío, tendrás que ordenar una lista de productos.
+
+Tu tarea es implementar la lógica de la función sortByAvailabilityAndPrice. Esta función recibirá un array de objetos que representan productos, y devolverá una copia ordenada de dicho array.
+
+El ordenamiento se realizará siguiendo dos criterios:
+
+    Primero, los productos disponibles en inventario serán colocados al principio de la lista.
+    Luego, los productos serán ordenados por su precio, de manera ascendente.
+
+Es importante destacar que la lista original no sufrirá ninguna modificación, y que la función devolverá una nueva lista con los cambios mencionados.*/
+export function sortByAvailabilityAndPrice(products) {
+    // Tu código aquí 👈
+    const productsSorted = [...products];
+    productsSorted.sort((a, b) => a.price - b.price)
+    productsSorted.sort((a, b) => b.inStock - a.inStock)
+    return productsSorted;
+  }
