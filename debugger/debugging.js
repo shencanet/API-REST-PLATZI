2c43d08ed242dcd67642e12fe0f7f0b3669901a3
shen <santi.dionis@gmail.com>
2023-03-12 21:25:33 +0100

    ADD: DEBIGGING

---
+++ debugger/debugging.js
@@ -0,0 +1,9 @@
+/*Debugging es el término para solucionar bugs. Los bugs (“bichos” en inglés) son errores en la aplicación, saber cómo solucionarlos de manera eficiente es clave para tu desarrollo como profesional.
+
+Todo navegador dispone de Dev tools o herramientas de desarrollador, que es un conjunto de características del código de la página web, una de estas es el debugging.
+
+La consola del navegador es importante para ver qué está pasando con el código generado. La consola se muestra con la combinación de teclas F12 / Ctrl + Shift + I / Cmd+Opt+I o clic derecho e “Inspeccionar” en tu navegador preferido (de preferencia Google Chrome).
+Código de ejemplo
+
+El código de ejemplo que se usará en las Dev Tools será el siguiente:*/
+
