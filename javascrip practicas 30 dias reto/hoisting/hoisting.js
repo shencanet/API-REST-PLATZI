17cb66f92cfa10b335faa68afc1a47b7ef90eaea
shen <santi.dionis@gmail.com>
2023-03-12 21:19:52 +0100

    ADD: HOISTING

--- javascrip practicas 30 dias reto/hoisting/hoisting.js
+++ javascrip practicas 30 dias reto/hoisting/hoisting.js
@@ -42,3 +42,62 @@ console.log( saludar() )
 function saludar() {
   return "Hola"
 }
+
+
+//Hoisting de variables dentro de una función
+/*
+El hoisting desplaza las declaraciones a la parte superior del scope más cercano, en el caso de una función dentro de otra función, seguiría el siguiente comportamiento.*/
+
+function scope() {
+  console.log(nombre) // undefined
+  console.log(edad) // undefined
+  console.log(i) // undefined
+  
+  var nombre = "Andres"
+  var edad = 20
+  for (var i = 0; i< 6; i++) {
+  //...
+  }
+}
+//JavaScript lo interpretaría como lo siguiente:
+
+function scope() {
+    var nombre = undefined
+    var edad = undefined
+    var i = undefined
+    
+    console.log(nombre) // undefined
+    console.log(edad) // undefined
+    console.log(i) // undefined
+    
+    nombre = "Andres"
+    edad = 20
+    for ( i = 0; i< 6; i++) {
+    
+    //
+    }
+  }
+  
+
+  //Hosting en funciones asignadas a variables
+/*
+Mira el siguiente código y piensa cuál sería el resultado del console.log.*/
+
+console.log( saludar() )
+
+var saludar = function saludar() {
+  return "hola"
+}
+
+
+/*
+La respuesta es un error de tipo porque si asignas una función a una variable declarada con var, y la invocas antes declararla, la variable será de tipo undefined y no de función por el hoisting.*/
+
+var saludar = undefined
+
+console.log( saludar() ) // TypeError: saludar is not a function
+
+saludar = function saludar() {
+  return "hola"
+}
+
