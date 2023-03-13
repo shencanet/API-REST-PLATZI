e68575f8cbe1b409b2da8a112cceb3c7c727a48c
shen <santi.dionis@gmail.com>
2023-03-13 19:25:55 +0100

    ADD: DEBuGGING

--- debugger/debugging.js
+++ debugger/debugging.js
@@ -7,3 +7,44 @@ Código de ejemplo
 
 El código de ejemplo que se usará en las Dev Tools será el siguiente:*/
 
+var a = "Hello global"
+
+function hello() {
+  let b = "Hello function"
+
+  if(true) {
+    let c = "Hello block"
+  }
+}
+
+hello()
+
+var a = "Hello global"
+
+function hello() {
+  let b = "Hello function"
+
+  if(true) {
+    let c = "Hello block"
+    debugger // <---- Palabra reservada para debbuging
+  }
+}
+
+[1, 2, 3].map((num) => {
+    if (typeof num === 'number') return;
+    return num * 2;
+  });
+  nameOfDog("Elmo"); function nameOfDog(name) { console.log(name); }; 
+/*
+  Respuesta Correcta: C) [undefined, undefined, undefined]
+  El método `map` es propio del paradigma de la programación funcional. Este método siempre retorna una nuevo arreglo.
+
+En el ejemplo, puesto que estamos iterando sobre un arreglo de números, la condición evaluaerá `true` para cada uno de los elementos del arreglo, pero hay 2 sentencias `return`. JavaScript ignora todo el código que esta después del primer `return` que encuntra. Dicho esto, tendríamos algo así:
+
+[1, 2, 3].map(num => { if (typeof num === "number") return; });
+
+Ahora, si bien la condición se evalua a `true`, el `return` no devuelve nada, simplemente hace que el código se salga del `map`.
+
+Cuando no devolvemos nada en `return`, `map` regresa siempre `undefined`.
+
+Al tener 3 elementos en el arreglo, y recordando siempre que map regresa un nuevo arreglo, obtenemos como resultado final un arreglo de 3 `undefined`*/
