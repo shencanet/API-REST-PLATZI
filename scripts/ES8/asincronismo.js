e266927f6bada6c9dddeeb43a3526376b0d569a9
shen <santi.dionis@gmail.com>
2023-03-14 21:20:23 +0100

    ADD: asincronismo

--- scripts/ES8/asincronismo.js
+++ scripts/ES8/asincronismo.js
@@ -32,3 +32,21 @@ const anotherFn = async () => {
 console.log("before");
 anotherFn();
 console.log("After");
+
+const fnAsync = () => {
+    return new Promise ((resolve, reject) =>{
+        (true)
+            ? setTimeout(() => resolve ('Luego de esperar 2 segundos'),2000)
+            :reject(new Error('Error!'));
+    });
+}
+
+const anotherFn = async() => {
+    const something = await fnAsync();
+    console.log(something);
+    console.log('Hello!');
+}
+
+console.log('Antes');
+anotherFn();
+console.log('Despues');
