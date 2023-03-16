3c8056b7698683ec01acef81ef757b5fec1b6382
shen <santi.dionis@gmail.com>
2023-03-16 19:32:00 +0100

    ADD: ES10 FLAT MAP

--- scripts/ES10/flat-map y trimStart-trimEnd.js
+++ scripts/ES10/flat-map y trimStart-trimEnd.js
@@ -45,3 +45,36 @@ El método flatMap es una combinación de los métodos map y flat. Primero reali
 Este método es inmutable, es decir, retorna un nuevo array con los cambios y no cambia el array original.
 
 Este método recibe los mismos argumentos que el método map.*/
+const strings = ["Nunca pares", "de Aprender"]
+strings.map(string => string.split(" ")) 
+// [ [ 'Nunca', 'pares' ], [ 'de', 'Aprender' ] ]
+strings.flatMap(string => string.split(" ")) 
+// [ 'Nunca', 'pares', 'de', 'Aprender' ]
+
+const numbers = [1,2, 3, 4]
+numbers.map(number => [number * 2]) 
+// [[2], [4], [6], [8]]
+numbers.flatMap(number => [number *2]) 
+// [2, 4, 6, 8]
+
+// Cuidado, primero hace el map y luego el flat
+const numbers2 = [1,[2,3], 4, 5]
+numbers2.flatMap(number => [number *2]) 
+// [ 2, NaN, 8, 10 ]
+// * Recuerda: NaN = No a Number
+/*Eliminar espacios en blanco de un string
+
+Existen tres métodos para eliminar espacios en blanco de un string:
+
+    El método trim elimina los espacios en blanco al inicio y al final.
+    El método trimStart o trimLeft elimina los espacios al inicio.
+    El método trimEnd o trimRight elimina los espacios al final.
+*/
+const saludo = "      hola      "
+const result1 = saludo.trim()
+const result2 = saludo.trimStart()
+const result3 = saludo.trimEnd()
+
+result1 // 'hola'
+result2 // 'hola      '
+result3 // '      hola'
