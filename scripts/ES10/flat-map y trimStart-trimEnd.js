505d03245d3c956d8c7dadf1b949ac9805f43f9c
shen <santi.dionis@gmail.com>
2023-03-16 19:31:13 +0100

    ADD: ES10 FLAT MAP

---
+++ scripts/ES10/flat-map y trimStart-trimEnd.js
@@ -0,0 +1,47 @@
+// flat
+const array = [1,1,2,3,4, [1,3,5,6,[1,2,4]]];
+console.log(array.flat(3));
+
+// flatmap
+const array2 = [1,2,3,4,5];
+console.log(array2.flatMap(v => [v, v *2]));
+const hello = "       Hello World!   ";
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
+/*Qué es el aplanamiento de arrays
+
+El aplanamiento consiste en transformar un array de arrays a una sola dimensión. Los métodos flat y flatMap permitirán realizar el aplanamiento.*/
+/*Método flat
+
+El método flat devuelve un array donde los sub-arrays han sido propagados hasta una profundidad especificada.
+
+Este método es inmutable, es decir, retorna un nuevo array con los cambios y no cambia el array original.
+
+Este método recibe un argumento:
+
+    La profundidad del aplanamiento, por defecto, tiene un valor de 1.
+
+Si se desea aplanar todos los sub-arrays en una sola dimensión, utiliza el valor de Infinity.*/
+const array = [1,2,[3,4],5,6]
+const result = array.flat() 
+result// [1,2,3,4,5,6]
+
+const array2 = [1, 2, [3, 4, [5, 6]]];
+const result2 = array2.flat() 
+result2// [1, 2, 3, 4, [5, 6]]
+
+const array3 = [1, 2, [3, 4, [5, 6]]]
+const result3 = array3.flat(2) 
+result3// [1, 2, 3, 4, 5, 6]
+
+const array4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]
+const result4 = array4.flat(Infinity) 
+result4// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/*Método flatMap
+
+El método flatMap es una combinación de los métodos map y flat. Primero realiza la iteración de los elementos del array (como si fuera map), y después los aplana en una sola profundidad (como si fuera flat).
+
+Este método es inmutable, es decir, retorna un nuevo array con los cambios y no cambia el array original.
+
+Este método recibe los mismos argumentos que el método map.*/
