66824303e968a41d867d3583e8b1f6da31dd2755
shen <santi.dionis@gmail.com>
2023-03-16 19:47:08 +0100

    ADD: ES11 try catch

---
+++ scripts/ES10/try catch y fromEntries.js
@@ -0,0 +1,21 @@
+/*Parámetro opcional de catch
+
+El parámetro opcional de catch permite omitir el error si es necesario*/
+
+try {
+    // Manejar el código
+  } catch (err) {
+    // Se utiliza el parámetro `err`
+  }
+  
+  try {
+    // Manejar el código
+  } catch {
+    // Manejar el error sin el parámetro.
+  }
+
+  /*Cómo transformar un array de arrays en un objeto
+
+El método Object.fromEntries devuelve un objeto a partir de un array donde sus elementos son las entries en forma [propiedad, valor].
+
+Se considera la operación inversa de Object.entries(*/
