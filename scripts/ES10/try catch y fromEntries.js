64fefb7d1cd0dbd1f814ad6bce7e5a664ef45344
shen <santi.dionis@gmail.com>
2023-03-17 20:25:36 +0100

    ADD: ES11 try catch

--- scripts/ES10/try catch y fromEntries.js
+++ scripts/ES10/try catch y fromEntries.js
@@ -19,3 +19,18 @@ try {
 El método Object.fromEntries devuelve un objeto a partir de un array donde sus elementos son las entries en forma [propiedad, valor].
 
 Se considera la operación inversa de Object.entries(*/
+const arrayEntries = [
+    [ 'name', 'Andres' ],
+    [ 'email', 'andres@correo.com' ],
+    [ 'age', 23 ]
+  ] 
+  
+  const usuario = Object.fromEntries(arrayEntries) 
+  
+  console.log(usuario)
+  /* {
+    name: 'Andres',
+    email: 'andres@correo.com',
+    age: 23
+  }
+  */
