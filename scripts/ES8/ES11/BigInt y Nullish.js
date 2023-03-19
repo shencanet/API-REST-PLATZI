f5cd634c3e301df3b518a3c8518c75f77f765aab
shen <santi.dionis@gmail.com>
2023-03-19 21:19:33 +0100

    ADD: ES11 BigINT

--- scripts/ES8/ES11/BigInt y Nullish.js
+++ scripts/ES8/ES11/BigInt y Nullish.js
@@ -20,3 +20,17 @@ const bigInt = BigInt(Number.MAX_SAFE_INTEGER) + BigInt(increment)
 
 console.log(number) // 9007199254740992
 console.log(bigInt) // 9007199254740993n
+/*Se añade la misma cantidad a ambos tipos de datos, sin embargo, el tipo numérico da un resultado diferente al esperado.
+Operador Nullish Coalescing
+
+El operador nullish coalescing (??) consiste en evaluar una variable si es undefined o null para asignarle un valor.
+
+El siguiente ejemplo se lee como: ¿usuario.name es undefined o null? Si es así, asígnale un valor por defecto "Andres", caso contrario asigna el valor de usuario.name.*/
+const usuario1 = {}
+const nombre1 = usuario1.name ?? "Andres"
+
+const usuario2 = {name: "Juan"}
+const nombre2 = usuario2.name ?? "Andres"
+
+console.log(nombre1) // 'Andres' 
+console.log(nombre2) // 'Juan'
