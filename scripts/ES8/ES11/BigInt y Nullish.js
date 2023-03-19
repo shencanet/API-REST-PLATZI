e917b192031cbf2a40354459df8448aa1d7f1817
shen <santi.dionis@gmail.com>
2023-03-19 21:19:54 +0100

    ADD: ES11 BigINT

--- scripts/ES8/ES11/BigInt y Nullish.js
+++ scripts/ES8/ES11/BigInt y Nullish.js
@@ -34,3 +34,17 @@ const nombre2 = usuario2.name ?? "Andres"
 
 console.log(nombre1) // 'Andres' 
 console.log(nombre2) // 'Juan'
+/*
+Diferencia entre el operador OR y el Nullish coalescing
+
+El operador OR (||) evalúa un valor falsey. Un valor falsy es aquel que es falso en un contexto booleano, estos son: 0, "" (string vacío), false, NaN, undefined o null.
+
+Puede que recibas una variable con un valor falsy que necesites asignarle a otra variable, que no sea null o undefined. Si evalúas con el operador OR, este lo cambiará, provocando un resultado erróneo.*/
+
+const id = 0
+
+const orId = id || "Sin id"
+const nullishId = id ?? "Sin id"
+
+console.log( orId ) //  'Sin id'
+console.log( nullishId ) // 0
