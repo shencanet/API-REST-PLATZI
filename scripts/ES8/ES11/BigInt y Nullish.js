62fb72bc9c90debd8db08672148406c333d381d3
shen <santi.dionis@gmail.com>
2023-03-19 21:19:17 +0100

    ADD: ES11 BigINT

--- scripts/ES8/ES11/BigInt y Nullish.js
+++ scripts/ES8/ES11/BigInt y Nullish.js
@@ -8,3 +8,15 @@ typeof 45n // 'bigint'
 /*
 JavaScript tiene límites numéricos, un máximo Number.MAX_SAFE_INTEGER y un mínimo Number.MIN_SAFE_INTEGER.*/
 
+const max = Number.MAX_SAFE_INTEGER
+const min = Number.MIN_SAFE_INTEGER
+
+console.log(max)  // 9007199254740991
+console.log(min) // -9007199254740991
+//Después de los límites, los cálculos muestran resultados erróneos. Los bigint ayudan a manejar operaciones de enteros fuera de los límites mencionados.
+const increment = 2
+const number = Number.MAX_SAFE_INTEGER + increment
+const bigInt = BigInt(Number.MAX_SAFE_INTEGER) + BigInt(increment)
+
+console.log(number) // 9007199254740992
+console.log(bigInt) // 9007199254740993n
