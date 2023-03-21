2e9f8b08a4bfd67344c408fc838058d16561bbfe
shen <santi.dionis@gmail.com>
2023-03-21 20:30:18 +0100

    ADD: Match All

--- scripts/ES11/globalThis y matchAll.js
+++ scripts/ES11/globalThis y matchAll.js
@@ -48,3 +48,15 @@ console.log(array)
   ]
 ]
 */
+const regex = /\b(Apple)+\b/g;
+
+const fruit = "Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.";
+
+for (const match of fruit.matchAll(regex)) {
+    console.log(match);
+}
+
+console.log(window); // navegador
+console.log(global);  // node
+console.log(selft);// webworker
+console.log(globalThis);
