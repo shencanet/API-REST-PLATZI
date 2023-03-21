f42eb874dd0236fd2fe7081146e795c9afd6b6e4
shen <santi.dionis@gmail.com>
2023-03-21 20:29:40 +0100

    ADD: Match All

--- scripts/ES11/globalThis y matchAll.js
+++ scripts/ES11/globalThis y matchAll.js
@@ -16,3 +16,35 @@ window === globalThis // true
 
 // En Node.js
 global === globalThis // true
+/* Método matchAll para expresiones regulares
+
+El método matchAll de los strings devuelve un iterable con todas las coincidencias del string específico a partir de una expresión regular, colocada como argumento.
+
+string.matchAll(regex)
+
+En el iterable, existe una propiedad denominada index con el índice del string donde la búsqueda coincide.*/
+const regex = /\b(Apple)+\b/g
+
+const fruit = "Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc."
+
+// Tranformación del iterable retornado a array
+const array = [...fruit.matchAll(regex)]
+console.log(array)
+/*
+[
+  [
+    'Apple',
+    'Apple',
+    index: 0,
+    input: 'Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.',
+    groups: undefined
+  ],
+  [
+    'Apple',
+    'Apple',
+    index: 21,
+    input: 'Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.',
+    groups: undefined
+  ]
+]
+*/
