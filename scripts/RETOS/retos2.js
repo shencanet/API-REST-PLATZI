49d6953fb7665c9e882199c54369eacdc48ab8d7
shen <santi.dionis@gmail.com>
2023-03-24 21:18:29 +0100

    ADD: Retos

---
+++ scripts/RETOS/retos2.js
@@ -0,0 +1,58 @@
+
+/*
+En este desafío, deberás crear tu propia implementación de filter para el prototype de los arrays.
+
+Esto implica agregar un nuevo método llamado myFilter al prototype de los arrays, el cual permitirá filtrar elementos de manera similar al método filter nativo del lenguaje. El objetivo es poder usar el método myFilter de la siguiente manera:
+
+Ejemplo 1:*/
+
+
+const array = [1,2,3,4,5,6]
+
+array.myFilter(num => num % 2 === 0)
+
+Output: [2,4,6]
+
+//Ejemplo 2:
+
+
+
+
+const arr = [
+  {
+    name: "Juan",
+    age: 10,
+  },
+  {
+    name: "Pedro",
+    age: 20,
+  },
+  {
+    name: "Maria",
+    age: 30,
+  },
+];
+
+array.myFilter((person) => person.age > 18)
+
+Output: [
+  {
+    name: "Pedro",
+    age: 20,
+  },
+  {
+    name: "Maria",
+    age: 30,
+  },
+]
+
+//mi resultado
+export function arrayModified() {
+    Array.prototype.myFilter = function (funcion) {
+      let arrayMap = [];
+      for (let prop of this) {
+        funcion(prop) ? arrayMap.push(prop) : prop;
+      }
+      return arrayMap;
+    }
+  }
