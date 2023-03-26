4011f4b9e4035f9b574b6e0c0e46b28a237a9be9
shen <santi.dionis@gmail.com>
2023-03-26 13:03:31 +0200

    ADD: Retos

--- scripts/RETOS/retos2.js
+++ scripts/RETOS/retos2.js
@@ -133,3 +133,6 @@ const rta = array1.map((item) => item.length);
 El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
 
 .map() es inmutable, por lo tanto, no modifica el array original, sino que crea uno nuevo con la “transformación” aplicada. Además, mantienes el mismo length que el array original, te devuelve en el nuevo array la misma cantidad que el array que le aplicaste el método.*/
+const timesThree = (num) => num * 3;
+/*Respuesta Correcta: A) Función Pura
+Una función pura es aquella que, dada una misma entrada, siempre regresa el mismo valor de salida y no tiene otro efecto secundario observable.*/
